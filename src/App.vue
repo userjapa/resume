1bcec2caa7c1990861af42495aebb602df44a610
usuarioJapa <willian.hsimabukuro@gmail.com>
2019-11-24 19:36:37 -0300

    lf to crlf

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,74 @@
 <template>
   <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
+    <main class="container wrap">
+      <div class="flex-grow-3">
+        <Menu/>
+      </div>
+      <div class="flex-grow-5 full">
+        <router-view></router-view>
+      </div>
     </main>
   </div>
 </template>
 
 <script>
+import Menu from '@/components/Menu';
+
 export default {
   name: 'app',
+  components: {
+    Menu,
+  },
+  mounted() {
+    const full = Array.from(document.getElementsByClassName('full'));
+    if (full) {
+      full.forEach((e) => {
+        e.style.maxHeight += `${window.innerHeight - 80}px`;
+      });
+    }
+  },
 };
 </script>
 
 <style>
+@import 'assets/css/the-grid.css';
+
 body {
   margin: 0;
+  padding: 0;
+  border: 0;
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Century Gothic', monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #516b57;
+  letter-spacing: .02em;
+}
+
+a {
+  text-decoration: none;
+  outline: none;
+  color: #516b57
+}
+
+a:active {
+  color: #aa998a;
+}
+
+a:hover {
+  cursor: pointer;
 }
 
 main {
   text-align: center;
-  margin-top: 40px;
+  margin: 40px;
 }
 
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
+.full {
+  height: 100%;
+  overflow: auto;
 }
 
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
 </style>
